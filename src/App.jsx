dbb1b2f1257fea409291d2e28d757036776678eb
emonShimoul <emonshimoul@gmail.com>
2025-03-27 19:15:06 +0600

    installation of tailwind and daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Byte & Blog</h1>
-    </>
+    <div className="text-center">
+      <h1 className="text-red-700 font-bold mt-10 mb-4">Byte & Blog</h1>
+      <span className="loading loading-infinity loading-xs"></span>
+      <span className="loading loading-infinity loading-sm"></span>
+      <span className="loading loading-infinity loading-md"></span>
+      <span className="loading loading-infinity loading-lg"></span>
+      <span className="loading loading-infinity loading-xl"></span>
+    </div>
   );
 }
 
