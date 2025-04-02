b8506cdc32e0bb7abdc90ec6c548bbe6fe201a32
emonShimoul <emonshimoul@gmail.com>
2025-04-02 18:27:45 +0600

    added some components

---
+++ src/pages/AllBlogs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllBlogs = () => {
+  return (
+    <div>
+      <h2>All Blogs</h2>
+    </div>
+  );
+};
+
+export default AllBlogs;
---
+++ src/pages/FeaturedBlogs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FeaturedBlogs = () => {
+  return (
+    <div>
+      <h2>Featured Blogs</h2>
+    </div>
+  );
+};
+
+export default FeaturedBlogs;
